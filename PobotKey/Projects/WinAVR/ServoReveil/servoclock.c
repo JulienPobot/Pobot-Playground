11be49eb46c5eac0c8feb7c251103ea192010c39
julien.holtzer <julien.holtzer@c631b436-7905-11de-aab9-69570e6b7754>
2010-02-27 08:34:29 +0000

    Utilisation de "delay" en réunion 26/02/10.

--- PobotKey/Projects/WinAVR/ServoReveil/servoclock.c
+++ PobotKey/Projects/WinAVR/ServoReveil/servoclock.c
@@ -2,6 +2,8 @@
     
 #include <avr/io.h>
 
+#include <util/delay.h>
+
 
 static void avr_init(void);
 
@@ -13,7 +15,15 @@ int main(void)
     for(;;)
     {
         // Tasks here.
-		  
+		
+		PORTB |= 0x01;
+		
+		_delay_ms(500);
+		
+		PORTB &= 0xFE;
+		
+		_delay_ms(250);
+		 
     }
     
     return(0);
@@ -24,11 +34,12 @@ int main(void)
 static void avr_init(void)
 {
     // Initialize device here.
+
 	
 	
 	// le servo
 	
-	
+	DDRB |= 0x01;
 	
 	
 	// l'interruption
