e4a4b047aaa455ab1bf87f5929d025aaae3f6a02
julien.holtzer <julien.holtzer@c631b436-7905-11de-aab9-69570e6b7754>
2010-02-28 15:40:11 +0000

    Différents projets de test (principalement du matériel) pour AVR

---
+++ PobotKey/Projects/WinAVR/TestHardware/TestCarteOdo/testodo.c
@@ -0,0 +1,92 @@
+//----- Include Files ---------------------------------------------------------
+#include <avr/io.h>		// include I/O definitions (port names, pin names, etc)
+#include <avr/interrupt.h>	// include interrupt support
+#include "global.h"		// include our global settings
+#include "timer.h"
+#include "i2c.h"		// include i2c function library
+
+#include "math.h"
+#include "stdio.h"
+#include "string.h"
+
+#include "lcd03.h"		// include lcd03
+
+/** Définition des constantes **/
+
+#define SLAVE_CODEUR_ADDR	0xA1
+
+/** Définition des variables **/
+
+// local data buffer
+unsigned char localBuffer[] = "01234567";
+unsigned char localBufferLength = 0x20;
+
+char* msg = "testtesttest";
+
+// Structure d'échange odométrie
+typedef struct 
+{
+	float dLeft;
+	float dRight;
+} ODO_MESSAGE, *ODO_MESSAGE_PTR;
+
+volatile ODO_MESSAGE	stOdo_Message;
+
+
+/** Signature des fonctions **/
+
+
+/**
+ * Programme principal du robot
+ */
+int main(void)
+{	
+	
+	/** Démarrage et initialisations **/
+	
+	/* initialisation des flags */
+	
+	/* initalisation des périphériques utilisés */
+	
+	timerInit();
+	i2cInit();	
+
+	
+	//
+	lcd03Init();
+	lcd03Display("reset permanent",1,1);
+	lcd03Reset();
+	
+	stOdo_Message.dLeft = 0;
+	stOdo_Message.dRight = 0;
+	//
+
+	/* initialisation des directions des ports */
+	
+	
+	/* initialisation des ports/bits de sortie */
+	
+	
+	/**  Boucle principale **/
+
+	while (1) 
+	{
+		
+		
+		i2cMasterReceive(SLAVE_CODEUR_ADDR, sizeof(stOdo_Message), localBuffer);
+		// afficher
+		lcd03Display(localBuffer,2,2);
+		// afficher
+		sprintf(msg,"G: %d | D: %d",stOdo_Message.dLeft,stOdo_Message.dLeft);
+		lcd03Display(msg,1,1);
+		
+		// attendre 50 ms
+		timerPause(1000);
+	}
+	
+	return 0;
+}
+
+
+/** Les fonctions utilitaires **/
+
---
+++ PobotKey/Projects/WinAVR/TestHardware/TestFuzzy/Fuzzy.c
@@ -0,0 +1,177 @@
+/*
+	Application name:	FUzzy Development and Generation Environment (FUDGE) Version V1.02
+	File name:			Fuzzy.c
+	Written by:			Alex DeCastro & Jason Spielman
+
+	Copyright Motorola 1994
+	SCALE	1
+*/
+
+#include <stdio.h>
+#include "Fuzzy.h"
+
+int	num_inputs = 2;
+int	num_outputs = 2;
+int	num_rules = 11;
+
+int	num_input_mfs[2] = { 4, 5 };
+
+struct	In	Inputs[] =
+{
+	{ 0.000000, 3600.000000 },
+	{ -180.000000, 180.000000 }
+};
+
+float	inmem_points[2][7][4] =
+{
+	{
+		{ 0.000000, 0.000000, 5.000000, 50.000000 },
+		{ 10.000000, 50.000000, 500.000000, 1000.000000 },
+		{ 500.000000, 1000.000000, 3000.000000, 3600.000000 },
+		{ 3000.000000, 3600.000000, 3600.000000, 3600.000000 }
+	},
+	{
+		{ -90.000000, 0.000000, 0.000000, 90.000000 },
+		{ -180.000000, -90.000000, -90.000000, 0.000000 },
+		{ 0.000000, 90.000000, 90.000000, 180.000000 },
+		{ -180.000000, -180.000000, -180.000000, -90.000000 },
+		{ 90.000000, 180.000000, 180.000000, 180.000000 }
+	}
+};
+int	num_output_mfs[2] = { 5, 5 };
+
+struct	Out	Outputs[] =
+{
+	{ -100.000000, 100.000000 },
+	{ -100.000000, 100.000000 }
+};
+
+float	outmem_points[2][7][4] =
+{
+	{
+		{ 0.000000 },
+		{ 50.000000 },
+		{ 100.000000 },
+		{ -50.000000 },
+		{ -100.000000 }
+	},	{
+		{ 0.000000 },
+		{ 50.000000 },
+		{ 100.000000 },
+		{ -50.000000 },
+		{ -100.000000 }
+	}
+};
+float	crisp_outputs[2] = { 0, 0};
+
+int	num_rule_ants[11] = { 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2};
+int	num_rule_cons[11] = { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2};
+
+struct	Rule	Rules[11] =
+{
+	{ { 0x00 }, { 0x80, 0x81 } },
+	{ { 0x10, 0x01 }, { 0x90, 0x91 } },
+	{ { 0x10, 0x09 }, { 0x80, 0x91 } },
+	{ { 0x10, 0x11 }, { 0x90, 0x81 } },
+	{ { 0x10, 0x19 }, { 0xa0, 0x91 } },
+	{ { 0x10, 0x21 }, { 0x90, 0xa1 } },
+	{ { 0x08, 0x01 }, { 0x88, 0x89 } },
+	{ { 0x08, 0x09 }, { 0x80, 0x89 } },
+	{ { 0x08, 0x11 }, { 0x88, 0x81 } },
+	{ { 0x08, 0x19 }, { 0x98, 0x89 } },
+	{ { 0x08, 0x21 }, { 0x88, 0x99 } }
+};
+
+void fuzzy_step(float *crisp_inputs, float *crisp_outputs)
+{
+	int             in_index,rule_index,out_index;
+	float   in_val;
+	for (in_index = 0;in_index < num_inputs;in_index++)
+	{
+		fuzzify_input(in_index,crisp_inputs[in_index]);
+	}
+	for (rule_index = 0;rule_index < num_rules;rule_index++)
+	{
+		eval_rule(rule_index);
+	}
+	for (out_index = 0;out_index < num_outputs;out_index++)
+	{
+		crisp_outputs[out_index] = defuzzify_output(out_index, crisp_inputs);
+	}
+}
+void fuzzify_input(int in_index,float in_val)
+{
+	int i;
+	for (i = 0;i < num_input_mfs[in_index];i++)
+	{
+		fuzzy_inputs[in_index][i] = get_membership_value(in_index,i,in_val);
+	}
+}
+float get_membership_value(int in_index,int mf_index,float in_val)
+{
+	if (in_val < inmem_points[in_index][mf_index][0]) return 0;
+	if (in_val > inmem_points[in_index][mf_index][3]) return 0;
+	if (in_val <= inmem_points[in_index][mf_index][1])
+	{
+		if (inmem_points[in_index][mf_index][0] == inmem_points[in_index][mf_index][1])
+			return 1;
+		else
+			return ((in_val - inmem_points[in_index][mf_index][0]) /
+					(inmem_points[in_index][mf_index][1] - inmem_points[in_index][mf_index][0]));
+	}
+	if (in_val >= inmem_points[in_index][mf_index][2])
+	{
+		if (inmem_points[in_index][mf_index][2] == inmem_points[in_index][mf_index][3])
+			return 1;
+		else
+			return ((inmem_points[in_index][mf_index][3] - in_val) /
+					(inmem_points[in_index][mf_index][3] - inmem_points[in_index][mf_index][2]));
+	}
+	return 1;
+}
+void eval_rule(int rule_index)
+{
+	int             in_index,out_index,mf_index,ant_index,con_index;
+	int     val;
+	float   rule_strength = 1;
+	for     (ant_index = 0;ant_index < num_rule_ants[rule_index];ant_index++)
+	{
+		val = Rules[rule_index].antecedent[ant_index];
+		in_index = (val & 0x07);
+		mf_index = ((val & 0x38) >> 3);
+		rule_strength = MIN(rule_strength,fuzzy_inputs[in_index][mf_index]);
+	}
+	rule_strengths[rule_index] = rule_strength;
+	for (con_index = 0;con_index < num_rule_cons[rule_index];con_index++)
+	{
+		val = Rules[rule_index].consequent[con_index];
+		out_index = (val & 0x03);
+		mf_index = ((val & 0x38) >> 3);
+		fuzzy_outputs[out_index][mf_index] = MAX(fuzzy_outputs[out_index][mf_index],
+			rule_strengths[rule_index]);
+	}
+}
+float defuzzify_output(int out_index,float *inputs)
+{
+	float           summ = 0;
+	float           product = 0;
+	float           temp1,temp2;
+	int             mf_index,in_index;
+	for (mf_index = 0;mf_index < num_output_mfs[out_index];mf_index++)
+	{
+		temp1 = fuzzy_outputs[out_index][mf_index];
+		temp2 = outmem_points[out_index][mf_index][0];
+		summ = summ + temp1;
+		product = product + (temp1 * temp2);
+		fuzzy_outputs[out_index][mf_index] = 0;
+	}
+	if (summ > 0)
+	{
+		crisp_outputs[out_index] = product / summ;
+		return crisp_outputs[out_index];
+	}
+	else
+	{		
+		return crisp_outputs[out_index];
+	}
+}
