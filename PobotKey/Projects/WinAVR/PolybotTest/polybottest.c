7b63ce63d0a4aa8b8dd97abb7b25b4935be243be
julien.holtzer <julien.holtzer@c631b436-7905-11de-aab9-69570e6b7754>
2010-03-06 13:40:37 +0000

    Programme de test d'une carte Polybot sous WinAVR

---
+++ PobotKey/Projects/WinAVR/PolybotTest/polybottest.c
@@ -0,0 +1,18 @@
+
+//----- Include Files ---------------------------------------------------------
+#include <avr/io.h>		// include I/O definitions (port names, pin names, etc)
+#include <avr/interrupt.h>	// include interrupt support
+
+#include "global.h"		// include our global settings
+#include "timer.h"		// include timer function library (timing, PWM, etc)
+#include "lcd.h"
+
+
+int main(void)
+{
+
+	
+
+
+	return 0;
+}
