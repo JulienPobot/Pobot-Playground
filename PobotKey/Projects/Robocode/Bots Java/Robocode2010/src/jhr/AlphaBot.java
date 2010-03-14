c3c17793ce46357e0f4a5215f2ca536a0a5435ad
julien.holtzer <julien.holtzer@c631b436-7905-11de-aab9-69570e6b7754>
2010-03-14 19:31:10 +0000

    Mise à jour des commentaires

--- PobotKey/Projects/Robocode/Bots Java/Robocode2010/src/jhr/AlphaBot.java
+++ PobotKey/Projects/Robocode/Bots Java/Robocode2010/src/jhr/AlphaBot.java
@@ -7,7 +7,7 @@ import robocode.util.Utils;
 /**
  * 
  * @author Julien pour Pobot
- *
+ * 
  */
 public class AlphaBot extends AdvancedRobot {
 
@@ -17,7 +17,8 @@ public class AlphaBot extends AdvancedRobot {
 	@Override
 	public void run() {
 
-		// Le robot, le canon et le radar peuvent tourner indépendemment les uns des autres
+		// Le robot, le canon et le radar peuvent tourner indépendemment les uns
+		// des autres
 		setAdjustRadarForRobotTurn(true);
 		setAdjustRadarForGunTurn(true);
 		setAdjustGunForRobotTurn(true);
@@ -27,6 +28,7 @@ public class AlphaBot extends AdvancedRobot {
 		turnRadarRightRadians(Double.POSITIVE_INFINITY);
 		do {
 			// si on a perdu le robot ennemi, on tourne le radar
+			// mais ça ne doit pas arriver.
 			turnRadarRight(45);
 		} while (true);
 	}
@@ -35,35 +37,57 @@ public class AlphaBot extends AdvancedRobot {
 	public void onScannedRobot(ScannedRobotEvent e) {
 
 		// Alignement du radar avec l'adversaire
-		// selon le principe "narrow lock" 
+		// selon le principe "narrow lock"
 		// décrit sur le wiki : http://robowiki.net/wiki/Radar
 
 		double radarTurn =
-		// Absolute bearing to target
+		// angle entre notre robot (heading) et l'angle duquel l'adversaire est
+		// vu (bearing)
 		getHeadingRadians() + e.getBearingRadians()
-		// Subtract current radar heading to get turn required
+		// moins l'angle déjà en cours du radar
 				- getRadarHeadingRadians();
 
+		// demander à tourner du bon angle
+		// en incluant un facteur (1.0, 1.9 ou 2.0)
+		// tel que décrit sur le wiki : http://robowiki.net/wiki/Radar
 		setTurnRadarRightRadians(1.9 * Utils.normalRelativeAngle(radarTurn));
 
-		// Tourner le gun
-			
-		double absoluteBearing = getHeadingRadians() + e.getBearingRadians();
-		setTurnGunRightRadians(Utils.normalRelativeAngle(absoluteBearing - 
-		    getGunHeadingRadians() + (e.getVelocity() * Math.sin(e.getHeadingRadians() - 
-		    absoluteBearing) / 13.0)));
-		setFire(3.0);
-	
-		// Se déplacer
+		// gestion du canon et du tir
+		// selon le modèle "linéaire non itératif"
+		// décrit sur le wiki : http://robowiki.net/wiki/Linear_Targeting
 
+		// l'angle entre notre robot et l'adversaire
+		double absoluteBearing = getHeadingRadians() + e.getBearingRadians();
+
+		// calcul de l'angle de rotation
+		double gunTurn = absoluteBearing // enlever l'angle déjà atteint par le
+				// canon
+				- getGunHeadingRadians()
+				// et prendre en compte la vitesse et la direction de la cible
+				// pour tirer vers sa possible prochaine position
+				+ (e.getVelocity()
+						* Math.sin(e.getHeadingRadians() - absoluteBearing) / 13.0);
+
+		// demander la rotation de l'angle calculer
+		setTurnGunRightRadians(Utils.normalRelativeAngle(gunTurn));
+
+		// tirer (au maximum)
+		setFire(3.0);
+
+		// gérer le déplacement du robot
+		// en appliquant le principe du mouvement oscillant direct
+		// décrit sur le wiki : http://robowiki.net/wiki/Oscillator_Movement
+		// et modifié pour être aléatoire
+
+		// demander le déplacement du robot
 		if (getDistanceRemaining() == 0) {
 			FORWARD = -FORWARD;
 			setAhead(185 * Utils.getRandom().nextInt() * FORWARD);
 		}
+
+		// demander la rotation du robot
 		setTurnRightRadians(e.getBearingRadians() + Math.PI / 2 - 0.5236
 				* FORWARD * (e.getDistance() > 200 ? 1 : -1));
 
-
 	}
-
 }
