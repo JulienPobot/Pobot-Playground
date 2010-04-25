dff16e4c2ba5acd7c665377873dd5cfe9ff6513e
julien.holtzer <julien.holtzer@c631b436-7905-11de-aab9-69570e6b7754>
2010-04-25 10:33:19 +0000

    Ajout des projets P5 de MarioChuck

---
+++ PobotKey/Projects/Processing/Projets complets/MarioChuck/MarioChuckDisplay/Articulation.java
@@ -0,0 +1,99 @@
+import processing.core.*;
+import java.lang.Math;
+
+public class Articulation {
+
+  final static int minIncrement = 1;
+
+  // les limites  
+  int minimum;
+  int maximum;
+
+  // la position (consigne et courante)
+  int position;
+  int consigne;
+
+  public Articulation(int minPos, int maxPos, int value) {
+    minimum = minPos;
+    maximum = maxPos;
+    position = value;
+    consigne = value;
+  } 
+
+  /**
+   * Corrige une valeur en fonction des 
+   */
+  private int corrige(int value) {
+    return Math.max(minimum,Math.min(maximum,value));
+  }
+
+  /**
+   * Met à jour la consigne avec une nouvelle valeur
+   */
+  public void updatePosition(int value) {    
+    consigne = corrige(value);
+  }
+
+  /**
+   *
+   */
+  public void incrementPosition(int incr) {
+    consigne += incr;      
+    consigne = corrige(consigne);
+  }
+
+  public int getProportionalPosition(double ratio) {
+    return (int) (position * ratio); 
+  }
+
+  /**
+   *
+   */
+  public void update() {
+    if (position == consigne) { 
+      return; 
+    }   
+    if (position < consigne) { 
+      position += minIncrement; 
+    } 
+    else { 
+      position -= minIncrement; 
+    }
+    position = corrige(position);
+  }
+
+  public void draw(PApplet p, char type, int x, int y) {
+    // mettre à jour la position selon la consigne
+    update();
+    // horizontal
+    if (type == 'h') {
+      p.fill(200);
+      p.rect(x,p.height-y,180,10);
+      p.fill(100);
+      p.rect(x,p.height-y,(position-minimum)*180/(maximum-minimum),10);      
+      p.fill(255,0,0);
+      p.rect(x+180*(consigne-minimum)/(maximum-minimum)-5,p.height-y,10,10);
+    } 
+    // vertical
+    else {
+      p.fill(200);
+      p.rect(x,p.height-y-180,10,180);
+      p.fill(100);
+      p.rect(x,p.height-y,10,-(position-minimum)*180/(maximum-minimum));  
+      p.fill(255,0,0);
+      p.rect(x,p.height-y-(consigne-minimum)*180/(maximum-minimum)-5,10,10);
+    }         
+  }
+  
+  public String toString() {
+     return minimum + " < " + position + " / " + consigne + " < " + maximum + "\n";
+  }
+}
+
+
+
+
+
+
+
+
---
+++ PobotKey/Projects/Processing/Projets complets/MarioChuck/MarioChuckDisplay/MarioScene.java
@@ -0,0 +1,55 @@
+import processing.core.*;
+
+public class MarioScene {
+
+  PApplet p;  
+
+  PImage[] images = new PImage[6];
+
+  Figure[] figures = new Figure[6];
+
+  int index = 0;
+
+  public MarioScene(PApplet pa) {
+    this.p = pa;
+
+  }  
+
+  public void sauvePosition(MarioSimu msi, String name) {
+    if (index >= 6) {
+      p.println("Error: no more free attitudes");
+      return;
+    }
+    Figure fig = new Figure(msi.mario);
+    fig.filename = name;
+    figures[index] = fig;
+    index++;
+  }
+
+  public void draw() {
+    p.smooth();
+    for (int i=0; i<6; i++) 
+    {
+      Figure fig = figures[i];
+      if (fig != null) {
+        // affichage du cadre
+        //p.fill(0,30*i,0);
+        //p.rect(10+85*i,10,75,75); 
+        // affichage de l'image
+        if (images[i] == null) {
+          PImage pi = p.loadImage(fig.filename);
+          pi.copy(110,110,250,250,0,0,520,520);
+          images[i] = pi;
+        }
+
+        p.image(images[i], 10+85*i, 10, 75, 75);
+
+      }      
+    }    
+    p.noSmooth();
+  }
+
+}
+
+
+
