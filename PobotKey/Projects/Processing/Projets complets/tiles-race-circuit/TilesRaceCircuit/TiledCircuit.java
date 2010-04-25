363f2301afaf55efbe7b8113b29b88bb7cdc88f1
julien.holtzer <julien.holtzer@c631b436-7905-11de-aab9-69570e6b7754>
2010-04-25 09:54:36 +0000

    Mise à jour des projets Processing

---
+++ PobotKey/Projects/Processing/Projets complets/tiles-race-circuit/TilesRaceCircuit/TiledCircuit.java
@@ -0,0 +1,60 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+import processing.core.*;
+
+
+public class TiledCircuit {
+
+  public List circuit;
+
+  public boolean closed = false;
+
+  public TiledCircuit() {
+    circuit = new ArrayList();
+  } 
+
+  public void addTile(Tile ti) {
+    circuit.add(ti); 
+  }
+
+  public boolean containsTile(Tile ti) {
+    return circuit.contains(ti); 
+  }
+
+  public void displayText() {
+    Iterator it = circuit.iterator(); 
+    while (it.hasNext()) {
+      Tile t = (Tile) it.next();
+      System.out.print("("); 
+      System.out.print(t.row+1);
+      System.out.print(","); 
+      System.out.print(t.column+1);
+      System.out.print(") "); 
+    }
+    System.out.println();    
+  }
+
+  public void draw(PApplet pa) {
+    System.out.println("Draw circuit : "+circuit.size());
+    Iterator it = circuit.iterator(); 
+    while (it.hasNext()) {
+      Tile t = (Tile) it.next();
+
+      pa.pushMatrix();
+      pa.translate(t.column*t.tileW,t.row*t.tileH,1);
+      pa.fill(255,0,0,100);
+      pa.stroke(255,0,0);
+      pa.strokeWeight(1);    
+      pa.rect(2,2,t.tileW-4,t.tileH-4);    
+      pa.popMatrix();
+    }    
+
+  }
+
+}
+
+
+
+
+
---
+++ PobotKey/Projects/Processing/Projets complets/tiles-race-circuit/TilesRaceCircuit/applet/TiledCircuit.java
@@ -0,0 +1,42 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+public class TiledCircuit {
+
+  public List circuit;
+
+  public boolean closed = false;
+
+  public TiledCircuit() {
+    circuit = new ArrayList();
+  } 
+
+  public void addTile(Tile ti) {
+    circuit.add(ti); 
+  }
+
+  public boolean containsTile(Tile ti) {
+    return circuit.contains(ti); 
+  }
+
+  public void displayText() {
+    Iterator it = circuit.iterator(); 
+    while (it.hasNext()) {
+      Tile t = (Tile) it.next();
+      System.out.print("("); 
+      System.out.print(t.row);
+      System.out.print(","); 
+      System.out.print(t.column);
+      System.out.print(") "); 
+    }
+    System.out.println();    
+  }
+
+
+}
+
+
+
+
