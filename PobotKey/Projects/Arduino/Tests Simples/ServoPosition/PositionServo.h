9e0efe182855baa819f3d438838a79f137e2e212
julien.holtzer <julien.holtzer@c631b436-7905-11de-aab9-69570e6b7754>
2010-05-10 16:05:13 +0000

    Correction et amélioration de la classe pour un servomoteur à retour d'informations.

--- PobotKey/Projects/Arduino/Tests Simples/ServoPosition/PositionServo.h
+++ PobotKey/Projects/Arduino/Tests Simples/ServoPosition/PositionServo.h
@@ -1,26 +1,58 @@
+/**
+ * PositionServo.h
+ *
+ * Classe fille de la bibliothèque "Servo"
+ * définissant les fonctions spécifiques au retour d'informations
+ *
+ * (c) 2009 - J. Holtzer pour Pobot 
+ */
+
 #ifndef PositionServo_h
 #define PositionServo_h
 
 #include "WProgram.h"
 
+// déclaration de la classe et de l'héritage de Servo
 class PositionServo : 
 public Servo
 {
-private:
-  int pinAnalog = 0;
 
-  int valLow = 0;
-  int valHigh = 1024;
+  // ajout des variables du retour d'informations
+private:
+
+  // la patte où est connecté le câble de retour d'info
+  int pinAnalog;
+
+  // le nombre de degrés total entre les deux positions extrema
+  int totalDegres;
+
+  // la valeur correspondant à la position la plus basse 
+  int valLow; 
+
+  // la valeur correspondant à la position la plus haute
+  int valHigh; 
 
 public:
+
   PositionServo() : 
   Servo() {
+    // patte ANALOG 0
+    pinAnalog = 0;
+    // 180 degrés de développement standard
+    totalDegres = 180;
+    // 205 = 1 volt pour 1024 = 5 volts (ADC 10 bits)
+    valLow = 205;
+    // 615 = 3 volts pour 1024 = 5 volts (ADC 10 bits)
+    valHigh = 615;
   };    
 
-  uint8_t attach(int pinArg) {  
-    this->Servo::attach(pinArg); 
+  // personnalisation des valeurs (connexion et débattement)
+  void set(int pinArg, int degres) {
+    pinAnalog = pinArg;
+    totalDegres = degres;
   };
 
+  // calibrage des positions extrema
   void calibrate(int level) { 
     if (level == LOW) {
       valLow = analogRead(pinAnalog);
@@ -30,10 +62,17 @@ public:
     }
   };
 
+  // affichage de la position (valeur analogique brute)
   int position() { 
-    return analogRead(pinAnalog)*; 
+    return analogRead(pinAnalog); 
   };
+
+  // affichage de la position (valeur en degrés entre 0 et total degrés)
+  int degres() {
+    return (position()-valLow)*totalDegres/(valHigh-valLow);   
+  }
 
 };
 
 #endif
+
